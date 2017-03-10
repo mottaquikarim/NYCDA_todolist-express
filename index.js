603f62010c731a54b10c0d6916953ffb4fe0c135
Mottaqui Karim <taqqui.karim@gmail.com>
2017-03-09 21:49:31 -0500

    added get and post to express routes

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 //include express
 const express = require('express');
 
+// grab db
+const low = require('lowdb');
+
 // static file server
 const serveStatic = require('serve-static');
+
+// body parser middleware
+const parser = require('body-parser');
 
 //create an express application
 const app = express();
 
+// instantiate db
+const db = low('./db.json');
+
+//parses requests with the content type of `application/json`
+app.use(parser.json());
+
 //define a route on `/hello/world`
-app.get('/api/todos,(request, response) => {
+app.get('/api/todos',(request, response) => {
     response.header('Content-Type', 'application/json');
-    response.send({greeting: 'hello, wrold'});
+    response.send(db.get('todos').value());
 });
 
+
+// post todos
+app.post('/api/todos', (request, response) => {
+	const requestBody = request.body;
+
+	// Add a post
+	db.get('todos').push({
+		id: Date.now(), 
+		data: requestBody,
+	}).write();
+
+	response.header('Content-Type', 'application/json');
+	response.send(db.get('todos').value());
+
+});
 
 app.use('/', serveStatic( 'public', {
 	'index': [ 'index.html' ]
